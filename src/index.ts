0c893bdcc15cd742bcc5002c31ea0d5b7708b9a3
hyakt <hyakt0@gmail.com>
2021-09-29 15:57:16 +0900

    add: src

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import * as BabelTypes from '@babel/types'
+import { PluginObj } from '@babel/core'
+
+export interface Babel {
+  types: typeof BabelTypes
+}
+
+export default function (babel: Babel): PluginObj {
+  const t = babel.types
+  return {
+    visitor: {
+      BinaryExpression(path) {
+        if (path.node.operator !== '===') {
+          return
+        }
+        path.node.left = t.identifier('hyakt')
+        path.node.right = t.identifier('dork')
+      }
+    }
+  }
+}
